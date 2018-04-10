44f769f2e24cd0f4c964b88ae25432d08266e411
couturecraigj <couturecraigj@gmail.com>
2018-04-10 13:20:43 -0400

    finished fixing console statements

--- server/dev.js
+++ server/dev.js
@@ -1,8 +1,9 @@
 import webpack from 'webpack';
+import logger from './logger';
 
 import config from '../tools/webpack/development.client';
 
 webpack(config, err => {
-  if (err) console.log(err);
+  if (err) logger.log(err);
   require('./index').createServer(); // eslint-disable-line global-require
 });

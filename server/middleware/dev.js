ac949a137bd99bea68e5c4ede786748cea1e79ff
couturecraigj <couturecraigj@gmail.com>
2018-04-18 10:06:05 -0400

    Printing Linting Errors to console using Webpack

--- server/middleware/dev.js
+++ server/middleware/dev.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, node/no-missing-require, no-useless-escape */
 import chokidar from 'chokidar';
+import chalk from 'chalk';
 
 const logger = require('../logger');
 
@@ -13,6 +14,31 @@ const config = require('../../tools/webpack/development.client');
 const express = require('express');
 
 const compiler = webpack(config);
+const friendlySyntaxErrorLabel = 'Syntax error:';
+function formatMessage(message) {
+  return (
+    message
+      // Make some common errors shorter:
+      .replace(
+        // Babel syntax error
+        'Module build failed: SyntaxError:',
+        friendlySyntaxErrorLabel
+      )
+      .replace(
+        // Webpack file not found error
+        /Module not found: Error: Cannot resolve 'file' or 'directory'/,
+        'Module not found:'
+      )
+      // Internal stacks are generally useless so we strip them
+      .replace(/^\s*at\s.*:\d+:\d+[\s\)]*\n/gm, '') // at ... ...:x:y
+      // Webpack loader names obscure CSS filenames
+      .replace('./~/css-loader!./~/postcss-loader!', '')
+  );
+}
+
+function isLikelyASyntaxError(message) {
+  return message.indexOf(friendlySyntaxErrorLabel) !== -1;
+}
 
 const func = app => {
   const watcher = chokidar.watch(
@@ -22,11 +48,73 @@ const func = app => {
     }
   );
 
-  compiler.plugin('done', function() {
+  compiler.plugin('done', function(stats) {
     logger.log('Clearing /client/ module cache from server');
     Object.keys(require.cache).forEach(id => {
       if (/[\/\\]client[\/\\]/.test(id)) delete require.cache[id];
     });
+    const hasErrors = stats.hasErrors();
+    const hasWarnings = stats.hasWarnings();
+    if (!hasErrors && !hasWarnings) {
+      logger.log(chalk.green('Compiled successfully!'));
+      logger.log('');
+      logger.log(
+        chalk.gray('Note that the development build is not optimized.')
+      );
+      logger.log(
+        chalk.gray(
+          'To create a production build, use ' +
+            chalk.cyan('npm run build') +
+            '.'
+        )
+      );
+      logger.log('');
+      return;
+    }
+    const json = stats.toJson({}, true);
+    let formattedErrors = json.errors.map(
+      message => 'Error in ' + formatMessage(message)
+    );
+    const formattedWarnings = json.warnings.map(
+      message => 'Warning in ' + formatMessage(message)
+    );
+    if (hasErrors) {
+      logger.log(chalk.red('Failed to compile.'));
+      logger.log();
+      if (formattedErrors.some(isLikelyASyntaxError)) {
+        // If there are any syntax errors, show just them.
+        // This prevents a confusing ESLint parsing error
+        // preceding a much more useful Babel syntax error.
+        formattedErrors = formattedErrors.filter(isLikelyASyntaxError);
+      }
+      formattedErrors.forEach(message => {
+        logger.log(message);
+        logger.log();
+      });
+      // If errors exist, ignore warnings.
+      return;
+    }
+    if (hasWarnings) {
+      logger.log(chalk.yellow('Compiled with warnings.'));
+      logger.log();
+      formattedWarnings.forEach(message => {
+        logger.log(message);
+        logger.log();
+      });
+      // Teach some ESLint tricks.
+      logger.log('You may use special comments to disable some warnings.');
+      logger.log(
+        'Use ' +
+          chalk.yellow('// eslint-disable-next-line') +
+          ' to ignore the next line.'
+      );
+      logger.log(
+        'Use ' +
+          chalk.yellow('/* eslint-disable */') +
+          ' to ignore all warnings in a file.'
+      );
+      logger.log();
+    }
   });
 
   watcher.on('ready', () => {
--- tools/webpack/base.client.js
+++ tools/webpack/base.client.js
@@ -49,6 +49,12 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint-loader'
+      },
+      {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {

a13ac715a63f564479b984f4fdeb1fab8ea20a6f
couturecraigj <couturecraigj@gmail.com>
2018-04-19 15:59:36 -0400

    Trying to get appveyor to cache

--- server/routes/graphql/context/index.js
+++ server/routes/graphql/context/index.js
@@ -1,9 +1,10 @@
 import { graphql } from 'graphql';
 import createLoader from './Loader';
 import createDb from '../../../models';
+import { createUploadDir, uploadDir } from '../schema/resolvers/functions';
 
 const context = async (req, schema) => {
-  const db = await createDb();
+  const [db] = await Promise.all([createDb(), createUploadDir(uploadDir)]);
   return {
     loader: createLoader(req.user, db),
     query: query => graphql(schema, query),

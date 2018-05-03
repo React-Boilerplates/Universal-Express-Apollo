44a3ffc8cd7b176ff88e290ddaa8094b026a2692
couturecraigj <couturecraigj@gmail.com>
2018-05-03 14:48:07 -0400

    Trying to prevent Error Handler from hanging up

--- server/render/Error.jsx
+++ server/render/Error.jsx
@@ -16,20 +16,22 @@ const RoutedError = props => (
 RoutedError.propTypes = {};
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) =>
-  Promise.resolve().then(async () => {
-    logger.log(err);
-    try {
-      const notice = (await logger.error(err)) || {};
+  Promise.resolve()
+    .then(async () => {
+      logger.log(err);
+      try {
+        const notice = (await logger.error(err)) || {};
 
-      const page = await getPage({
-        plain: true,
-        req,
-        component: <RoutedError errorId={notice.id} />
-      });
-      return res.send(Html(...page));
-    } catch (error) {
-      return res.status(404).send("Sorry can't find that!");
-    }
-  });
+        const page = await getPage({
+          plain: true,
+          req,
+          component: <RoutedError errorId={notice.id} />
+        });
+        return res.send(Html(...page));
+      } catch (error) {
+        return res.status(404).send("Sorry can't find that!");
+      }
+    })
+    .catch(() => res.status(404).send("Sorry can't find that!"));
 
 export default RoutedError;
